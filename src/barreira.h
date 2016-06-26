a3f8ef317a31706cf57b07511e4c774cb0f43e29
Pedro Rocha <pedr0_r@hotmail.com>
2016-06-26 20:28:36 -0300

    TreeBarrier

--- src/barreira.h
+++ src/barreira.h
@@ -2,15 +2,25 @@
 #define BARREIRA_H
 
 #include <stdio.h>
+#include <stdatomic.h>
+#include <stdbool.h>
 
-typedef struct {
-	
-	
-} nodeBarreira;
+typedef struct nodeBarreira nodeBarreira;
 
+struct nodeBarreira{
+	atomic_int contador;
+	nodeBarreira *pai;
+	volatile bool sense;	
+};
 
-void inicializaBarreira();
-void barreiraWait();
-void barreiraDestroy();
+typedef struct{
+	int radix;
+	nodeBarreira **folha;
+	int folhas;
+} barreira;
+
+void inicializaBarreira(barreira *barreira, int radix, int numFolhas);
+void barreiraWait(barreira *barreira);
+void destroiBarreira(barreira *barreira);
 
 #endif
